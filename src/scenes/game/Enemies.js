1f032e52b2417df3af0afe5cdf25999ddf074ded
Max Sprenkels <101631891+MaxSprenkels@users.noreply.github.com>
2025-04-17 14:51:47 +0200

    Begin van het spel moeilijker maken

--- src/scenes/game/Enemies.js
+++ src/scenes/game/Enemies.js
@@ -3,41 +3,91 @@ export default class Enemies {
         this.scene = scene;
         this.group = this.scene.physics.add.group();
         this.stars = this.scene.physics.add.group();
+        this.lastEnemyY = null;
+        this.activeStars = 0;
+        this.maxActiveStars = 1;
 
         this.scene.time.addEvent({
-            delay: 1500,
+            delay: this.getCurrentEnemyDelay(),
             callback: this.createEnemy,
+            callbackScope: this,
+            loop: true
+        });
+
+        this.scene.time.addEvent({
+            delay: Phaser.Math.Between(10000, 15000),
+            callback: this.tryCreateStar,
+            callbackScope: this,
+            loop: true
+        });
+
+        this.scene.time.addEvent({
+            delay: Phaser.Math.Between(20000, 30000),
+            callback: this.tryCreatePowerUp,
             callbackScope: this,
             loop: true
         });
     }
 
-    createEnemy() {
-        const enemyTypes = ['enemyBlack1', 'enemyBlue1', 'enemyGreen1', 'enemyRed1'];
+    getCurrentEnemyDelay() {
+        const score = this.scene.score;
+        if (score < 20) return 600;
+        if (score < 40) return 500;
+        if (score < 60) return 400;
+        if (score < 80) return 300;
+        return 200;
+    }
+
+    tryCreateStar() {
+        if (this.activeStars >= this.maxActiveStars) return;
+
         const starTypes = ['star_silver', 'star_gold'];
         const randomY = Phaser.Math.Between(60, this.scene.scale.height - 60);
-        const rand = Phaser.Math.Between(1, 8);
-        const isStar = rand === 1;
-        const isMagnet = rand === 2;
-        const isShield = rand === 3;
+        const starType = Phaser.Utils.Array.GetRandom(starTypes);
 
-        if (isStar) {
-            const starType = Phaser.Utils.Array.GetRandom(starTypes);
-            const star = this.stars.create(this.scene.scale.width + 50, randomY, starType)
-                .setOrigin(0.5)
-                .setScale(1)
-                .setDepth(50)
-                .setVelocityX(-300)
-                .setData('points', starType === 'star_gold' ? 10 : 5);
+        const star = this.stars.create(this.scene.scale.width + 50, randomY, starType)
+            .setOrigin(0.5)
+            .setScale(1)
+            .setDepth(50)
+            .setVelocityX(-300)
+            .setData('points', starType === 'star_gold' ? 10 : 5)
+            .setSize(starType.width, starType.height);
 
-            star.setSize(star.width, star.height);
-        }
-        else if (isMagnet || isShield) {
-            const type = isMagnet ? 'magnet' : 'shield';
-            this.scene.powerUps.createPowerUp(this.scene.scale.width + 50, randomY, type);
-        }
-        else {
-            const enemy = this.group.create(this.scene.scale.width + 50, randomY, Phaser.Utils.Array.GetRandom(enemyTypes))
+        this.activeStars++;
+
+        star.on('destroy', () => {
+            this.activeStars--;
+        });
+    }
+
+    tryCreatePowerUp() {
+        const powerUpTypes = ['magnet', 'shield'];
+        const randomY = Phaser.Math.Between(60, this.scene.scale.height - 60);
+        const type = Phaser.Utils.Array.GetRandom(powerUpTypes);
+        this.scene.powerUps.createPowerUp(this.scene.scale.width + 50, randomY, type);
+    }
+
+    createEnemy() {
+        const enemyTypes = ['enemyBlack1', 'enemyBlue1', 'enemyGreen1', 'enemyRed1'];
+        let randomY;
+
+        do {
+            randomY = Phaser.Math.Between(60, this.scene.scale.height - 60);
+        } while (this.lastEnemyY && Math.abs(randomY - this.lastEnemyY) < 100);
+
+        this.lastEnemyY = randomY;
+
+        const rand = Phaser.Math.Between(1, 10);
+
+        if (rand === 1) {
+            if (Phaser.Math.Between(1, 3) === 1) {
+                this.tryCreateStar();
+            } else {
+                this.tryCreatePowerUp();
+            }
+        } else {
+            const enemy = this.group.create(this.scene.scale.width + 50, randomY,
+                Phaser.Utils.Array.GetRandom(enemyTypes))
                 .setOrigin(0.5)
                 .setScale(0.8)
                 .setDepth(50)
